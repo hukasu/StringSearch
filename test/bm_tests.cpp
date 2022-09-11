9b7670d6e721b76da3d33541238cca24441360c3
Lucas Farias <lucasfnf@gmail.com>
2022-09-11 14:34:33 +0900

    Add tests for Boyer-Moore

---
+++ test/bm_tests.cpp
@@ -0,0 +1,64 @@
+/**
+ * @file naive_tests.cpp
+ * @author Lucas Farias (lucasfnf@gmail.com)
+ * @brief Tests for Naive string search
+ * @version 0.1
+ * @date 2022-09-08
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+#include <sstream>
+
+#include "bm.hpp"
+
+TEST_CASE("String search test 1") {
+    std::unique_ptr<std::stringstream> text = std::make_unique<std::stringstream>(
+        "ABC ABCDAB ABCDABCDABDE",
+        std::ios::in | std::ios::binary
+    );
+    std::unique_ptr<std::stringstream> pattern = std::make_unique<std::stringstream>(
+        "ABCDABD",
+        std::ios::in | std::ios::binary
+    );
+
+    string_search::BoyerMooreStringSearch bm(std::move(text), std::move(pattern));
+    
+    std::optional<size_t> result = bm.next();
+
+    if (result.has_value()) CHECK_EQ(result.value(), 15);
+    else FAIL(1);
+}
+
+TEST_CASE("String search test 2") {
+    std::unique_ptr<std::stringstream> text = std::make_unique<std::stringstream>(
+        "TETTE TERETTE",
+        std::ios::in | std::ios::binary
+    );
+    std::unique_ptr<std::stringstream> pattern = std::make_unique<std::stringstream>(
+        "TE",
+        std::ios::in | std::ios::binary
+    );
+
+    string_search::BoyerMooreStringSearch bm(std::move(text), std::move(pattern));
+    
+    std::optional<size_t> result = bm.next();
+    if (result.has_value()) CHECK_EQ(result.value(), 0);
+    else FAIL(1);
+
+    result = bm.next();
+    if (result.has_value()) CHECK_EQ(result.value(), 3);
+    else FAIL(1);
+
+    result = bm.next();
+    if (result.has_value()) CHECK_EQ(result.value(), 6);
+    else FAIL(1);
+
+    result = bm.next();
+    if (result.has_value()) CHECK_EQ(result.value(), 11);
+    else FAIL(1);
+}
